995a9b77e94ecaa4b0f0f3f01f7e5379961152bf
Milav Dabgar <milav.dabgar@gmail.com>
2025-04-05 14:56:30 +0530

    users page improved much

--- app/routes/admin.py
+++ app/routes/admin.py
@@ -139,21 +139,27 @@ def delete_department(dept_id):
 @bp.route('/users', methods=['GET'])
 @roles_required('admin')
 def users():
+    # Get all users
+    users = User.query.order_by(User.created_at.desc()).all()
+    
     # Get role counts for the stats cards
     role_counts = {}
-    for role in Role.query.all():
+    roles = Role.query.all()
+    for role in roles:
         role_counts[role.name] = User.query.join(User.roles).filter(Role.name == role.name).count()
-
-    # Get recent users for the table
-    recent_users = User.query.order_by(User.created_at.desc()).limit(10).all()
+    
+    # Get all departments for filtering
+    departments = Department.query.all()
     
     # Create form instance for the modal
     form = UserCreationForm()
 
     return render_template('admin/users.html', 
+                         users=users,
+                         roles=roles,
+                         departments=departments,
                          form=form,
-                         role_counts=role_counts,
-                         recent_users=recent_users)
+                         role_counts=role_counts)
 
 @bp.route('/users/create', methods=['GET', 'POST'])
 @roles_required('admin')
